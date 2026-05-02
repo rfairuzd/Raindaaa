a21e84b07aa39d84b5cf5d52fdec339619adceb6
rfairuzd <rafifairuz71@gmail.com>
2026-05-03 02:48:56 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,29 @@
-const CACHE = 'rainda-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'rainda-v2';
+const ASSETS = [
+  '/Rainda/',
+  '/Rainda/index.html',
+  '/Rainda/manifest.json',
+  '/Rainda/sw.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
