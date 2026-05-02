b2044101980c2ef8defbf3f79b6906ae0d958711
rfairuzd <rafifairuz71@gmail.com>
2026-05-03 03:02:55 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'rainda-v3';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
